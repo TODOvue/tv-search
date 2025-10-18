b592d855863a15b686225d454791666e73953c9f
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-17 22:59:16 -0500

    feat(TvSearch): add plugin installation method and clean up main.js

--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,15 @@
 import TvSearch from './components/TvSearch.vue'
 
+(TvSearch as any).install = (app: any) => {
+  app.component('TvSearch', TvSearch)
+};
+
+export const TvSearchPlugin = {
+  install(app: any) {
+    app.component('TvSearch', TvSearch)
+  }
+}
+
+
 export { TvSearch }
 export default TvSearch
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import TvSearch from './demo/Demo.vue'
-import 'github-markdown-css'
 
 createApp(TvSearch).mount('#tv-search')
